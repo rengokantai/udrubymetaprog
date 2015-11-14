e4b5b0f78baad4ded9da29ef5e0bff25aa5b3605
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-14 17:57:34 -0500

    5-5 step2-1 may pollute global

---
+++ l5_5cont2.rb
@@ -0,0 +1,21 @@
+global_var = "global"
+
+OtherClass = Class.new do
+  puts global_var
+end
+
+SomeClass = Class.new do
+  local_var = "local"
+  define_method :foo do
+    global_var
+  end
+
+  define_method :bar do
+    local_var = "new"
+  end
+
+end
+
+
+obj = SomeClass.new
+puts obj.foo

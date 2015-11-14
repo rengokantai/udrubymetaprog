9291a6fd521290249d958eb00939fb37668e73f5
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-14 18:00:05 -0500

    5-5 add lambda

--- l5_5cont2.rb
+++ l5_5cont2.rb
@@ -1,21 +1,23 @@
-global_var = "global"
 
-OtherClass = Class.new do
-  puts global_var
-end
+lambda do
+  global_var = "global"
 
-SomeClass = Class.new do
-  local_var = "local"
-  define_method :foo do
-    global_var
+  OtherClass = Class.new do
+    puts global_var
   end
 
-  define_method :bar do
-    local_var = "new"
+  SomeClass = Class.new do
+    local_var = "local"
+    define_method :foo do
+      global_var
+    end
+
+    define_method :bar do
+      local_var = "new"
+    end
+
   end
-
-end
-
+end.call
 
 obj = SomeClass.new
 puts obj.foo
